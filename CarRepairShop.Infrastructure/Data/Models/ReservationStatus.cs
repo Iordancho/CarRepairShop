d80f2c133eb236203ef6a4515c80d64b84ce5a79
iordan <xhimmitter@gmail.com>
2024-03-02 18:57:41 +0200

    ReservationStatus entity added

---
+++ CarRepairShop.Infrastructure/Data/Models/ReservationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRepairShop.Infrastructure.Data.Models
+{
+    public class ReservationStatus
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public IList<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
