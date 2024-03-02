a3b43ed6b2dbc1a985950938f6a01c59103a18e5
iordan <xhimmitter@gmail.com>
2024-03-02 18:24:09 +0200

    Creating CarMake, CarModel entities

---
+++ CarRepairShop.Infrastructure/Data/Models/CarMake.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRepairShop.Infrastructure.Data.Models
+{
+    public class CarMake
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
