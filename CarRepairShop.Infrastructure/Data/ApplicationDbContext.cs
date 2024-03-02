b757b409431f27c3aa1497866c42dedd1f13ff3d
iordan <xhimmitter@gmail.com>
2024-03-02 18:35:11 +0200

    Creating DbSets

--- CarRepairShop.Infrastructure/Data/ApplicationDbContext.cs
+++ CarRepairShop.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CarRepairShop.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRepairShop.Infrastructure.Data
@@ -9,5 +10,12 @@ namespace CarRepairShop.Infrastructure.Data
             : base(options)
         {
         }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarMake> CarMakes { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+        public DbSet<RepairShop> RepairShops { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
     }
 }
