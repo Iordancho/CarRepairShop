dc29769d7764bb283d3c4a845efadb97b980ed41
iordan <xhimmitter@gmail.com>
2024-03-10 01:05:51 +0200

    creating ViewModels for AddCar action

---
+++ CarRepairShop.Core/Models/CarFormViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CarRepairShop.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static CarRepairShop.Infrastructure.Data.DataConstants;
+
+
+namespace CarRepairShop.Core.Models
+{
+    public class CarFormViewModel
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(CarModelMax, MinimumLength = CarModelMin, ErrorMessage = StringLenghtErrorMessage)]
+        public string Model { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public string ProductionYear { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(VINMax, ErrorMessage = VINLengthErrorMessage)]
+        public string VIN { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public int MakeId { get; set; }
+        public IEnumerable<MakeViewModel> Makes { get; set; } = new List<MakeViewModel>();
+    }
+}
