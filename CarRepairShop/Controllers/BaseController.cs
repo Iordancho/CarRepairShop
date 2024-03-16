a97cb520265651193c320bd47010eb718296b23a
iordan <xhimmitter@gmail.com>
2024-03-16 17:45:22 +0200

    BaseController added

---
+++ CarRepairShop/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRepairShop.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
