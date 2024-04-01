2b05fb68b6da3bdc3b4ee0649004e2d97b8aeb3c
iordan <xhimmitter@gmail.com>
2024-04-01 13:52:54 +0300

    adding custom pages for 404 and 500 error pages

--- CarRepairShop/Controllers/HomeController.cs
+++ CarRepairShop/Controllers/HomeController.cs
@@ -24,9 +24,13 @@ namespace CarRepairShop.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(statusCode == 404)
+            {
+                return View("Error404");
+            }
+            return View();
         }
     }
 }
